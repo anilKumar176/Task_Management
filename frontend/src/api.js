5ab088917faf619c4817d66b5804da9c18781100
Anil Kumar <anilkumarchavan76@gmail.com>
2026-04-05 03:22:51 +0530

    manage

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 //connect to backend
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://task-management-2vie.onrender.com"
 });
